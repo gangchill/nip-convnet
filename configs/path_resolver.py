5e7ad104db1f06719277f73dfdfdaad84596ed7c
Sabbir Ahmmed <sabbir.ahmmed@gmail.com>
2017-07-01 15:51:28 +0200

    worked on todos

---
+++ configs/path_resolver.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+import sys, os
+for path in Path(__file__).resolve().parents:
+    if path.name == 'nip-convnet':
+        sys_path = str(path)
+        break
+sys.path.append(sys_path)
+print(sys_path)
