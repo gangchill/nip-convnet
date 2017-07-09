6d5cb61fc5b79b05ae03fa5ccace1ce5656fc2ef
max <maximilian.kohlbrenner@gmx.de>
2017-07-10 00:14:10 +0200

    Added training files for the reference cnn - to be verified on the server

---
+++ experiments/paper_reference_experiments/paper_reference_cnn/train_paper_reference_cnn.sh
@@ -0,0 +1,25 @@
+#!/bin/bash          
+echo 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+echo 'XXXXXXXXXXXXXXXXX TRAIN REFERENCE CNN XXXXXXXXXXXXXXXX'
+echo 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+
+# current folder:
+PARENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TRAINING_ROOT_DIR=$PARENT_DIR
+
+echo "Working in $PARENT_DIR"
+
+
+DATASET="MNIST_SMALL"
+CNN_LOG_FOLDER="1k_MNIST_paper_reference_model"
+CNN_CONFIG_FILE_PATH="$PARENT_DIR/paper_reference_cnn.ini"
+TEST_SET_BOOL=false
+
+echo "Begin training the reference network"
+
+INIT_MODE="default"
+WEIGHTS_PATH="None"
+CNN_RUN_NAME="reference_cnn_sslr_ib"
+
+
+python train_and_test_cnn.py $DATASET $CNN_CONFIG_FILE_PATH $INIT_MODE $WEIGHTS_PATH $CNN_LOG_FOLDER $CNN_RUN_NAME $TEST_SET_BOOL
