cec34df6ff42b4b95352de3378c6e942ca56fb81
maxkohlbrenner <maximilian.kohlbrenner@gmx.de>
2017-07-11 15:03:23 +0000

    added multiple runs to mnist config

--- experiments/paper_reference_experiments/paper_reference_cnn/mnist/evaluate_pre_training.sh
+++ experiments/paper_reference_experiments/paper_reference_cnn/mnist/evaluate_pre_training.sh
@@ -7,12 +7,15 @@ echo 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 PARENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TRAINING_ROOT_DIR=$PARENT_DIR
 
+REF_RUN_NUM=5
+PRE_RUN_NUM=5
+
+
 echo "Working in $PARENT_DIR"
 
 DATASET="MNIST_SMALL"
-CNN_LOG_FOLDER="1k_mnist_final"
+CNN_LOG_FOLDER="1k_mnist_multi"
 CNN_CONFIG_FILE_PATH="$PARENT_DIR/paper_reference_cnn.ini"
-# CNN_CONFIG_FILE_PATH="experiments/experimental_templates/cnn_in_folder_demo/relu_cnn.ini"
 
 # if set to true, a test run using the test set is performed after the training
 TEST_SET_BOOL=True
@@ -21,17 +24,30 @@ echo "Begin training the reference network"
 
 INIT_MODE="resume"
 WEIGHTS_PATH="None"
-CNN_RUN_NAME="reference-random_init"
 
+RUN_PREFIX="reference-random_init"
 
-python train_and_test_cnn.py $DATASET $CNN_CONFIG_FILE_PATH $INIT_MODE $WEIGHTS_PATH $CNN_LOG_FOLDER $CNN_RUN_NAME $TEST_SET_BOOL $PARENT_DIR 
+for (( i=1; i<=${REF_RUN_NUM}; i++ ))
+do
+	echo "Begin training reference network $i"
+	RUN_NAME="${RUN_PREFIX}_${i}"  
+	
+	echo "Run name is: $RUN_NAME"
+
+	python train_and_test_cnn.py $DATASET $CNN_CONFIG_FILE_PATH $INIT_MODE $WEIGHTS_PATH $CNN_LOG_FOLDER $RUN_NAME $TEST_SET_BOOL $PARENT_DIR 
+done
+
 
 echo "Begin training the pre-trained network"
 
 INIT_MODE="pre_trained_encoding"
 WEIGHTS_PATH="experiments/paper_reference_experiments/paper_reference_cae/weights/mnist_paper_net_cae_2/paper_cae_test_0.05"
-CNN_RUN_NAME="pre-trained"
+RUN_PREFIX="pre-trained"
 
 
-python train_and_test_cnn.py $DATASET $CNN_CONFIG_FILE_PATH $INIT_MODE $WEIGHTS_PATH $CNN_LOG_FOLDER $CNN_RUN_NAME $TEST_SET_BOOL $PARENT_DIR
-
+for (( i=1; i<=${PRE_RUN_NUM}; i++ ))
+do
+	echo "Begin training pre-trained network  $i"
+	RUN_NAME="${RUN_PREFIX}_${i}"  
+	python train_and_test_cnn.py $DATASET $CNN_CONFIG_FILE_PATH $INIT_MODE $WEIGHTS_PATH $CNN_LOG_FOLDER $RUN_NAME $TEST_SET_BOOL $PARENT_DIR 
+done
