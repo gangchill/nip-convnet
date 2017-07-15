814875881d686da0c36f0855e8737e32853676b4
Russell Hofmann <russellhofmann@gmail.com>
2017-07-15 15:40:50 +0200

    Update train_and_test_cae for CK+

--- train_and_test_cae.py
+++ train_and_test_cae.py
@@ -162,7 +162,7 @@ def main():
 
 	elif DATASET == "CKPLUS":
 		import scripts.load_ckplus as load_ckplus
-		dataset = load_ckplus.read_data_sets(split=False, one_hot=True, frames=None)
+		dataset = load_ckplus.read_data_sets(split=False, one_hot=True, frames=100)
 		input_size = (49, 64)
 		num_classes = load_ckplus.NUM_CLASSES
 		nhwd_shape = False
